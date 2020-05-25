7270a15e9808ba9ddb347008ded7420c7d6c8cc2
Aneel Marshall <aneelmarshall@outlook.com>
2020-05-25 10:20:40 +0100

    add plugins to optimise and service workers for offline caching

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,20 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    fetch('http://localhost:8081/test')
+        .then(res => res.json())
+        .then(function (res) {
+            document.getElementById('results').innerHTML = res.message
+        })
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+            document.getElementById('weather').innerHTML = json.title
+        })
 }
 
 export { handleSubmit }
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,18 +3,25 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
